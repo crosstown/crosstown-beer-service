46f8d1a77415423197e3c7828f8503463b23f5fb
F.S.(Crosstown) <royalplanet2009@gmail.com>
2019-08-22 21:22:02 -0400

    Adding bootstrap . closes #7.

---
+++ src/main/java/net/fernandosimon/crosstownbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,49 @@
+package net.fernandosimon.crosstownbeerservice.bootstrap;
+
+import net.fernandosimon.crosstownbeerservice.domain.Beer;
+import net.fernandosimon.crosstownbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * @author developer
+ * @date 2019-08-22
+ * @time 21:09
+ */
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        if (beerRepository.count() == 0) {
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+
+        }
+    }
+}
