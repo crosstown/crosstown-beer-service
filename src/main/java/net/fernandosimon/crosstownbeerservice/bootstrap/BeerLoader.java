f8e66bb0e55a38bb97fb509a91c280d9dfb84609
F.S.(Crosstown) <royalplanet2009@gmail.com>
2019-08-23 20:24:20 -0400

    Adding validation. Closes #8.

--- src/main/java/net/fernandosimon/crosstownbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/net/fernandosimon/crosstownbeerservice/bootstrap/BeerLoader.java
@@ -4,7 +4,11 @@ import net.fernandosimon.crosstownbeerservice.domain.Beer;
 import net.fernandosimon.crosstownbeerservice.repositories.BeerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.math.BigDecimal;
 
 /**
@@ -46,4 +50,5 @@ public class BeerLoader implements CommandLineRunner {
 
         }
     }
+
 }
